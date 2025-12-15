821ee496bb9602104509e0914ae578c5931d5f72
Noyon-34 <noyonmozumder983@gmail.com>
2025-12-16 02:09:25 +0600

    daily cp update

--- push.bat
+++ push.bat
@@ -1,10 +1,4 @@
 @echo off
-DATE=$(date "+%Y-%m-%d %H:%M:%S")
-
 git add .
-
-GIT_AUTHOR_DATE="$DATE" \
-GIT_COMMITTER_DATE="$DATE" \
-git commit -m "daily cp practice"
-
+git commit -m "daily cp update"
 git push
