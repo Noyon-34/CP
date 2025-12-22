d0e3f2c19d182ea84aaf9850775a6be95b05e810
Noyon-34 <noyonmozumder983@gmail.com>
2025-12-22 21:28:13 +0600

    Solved problem  in 22.12.25

---
+++ Problems/Graph Theory/Equalizing Money.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e4 + 7;
+using ll = long long;
+
+vector<int> money;
+vector<int> graph[N];
+bool vis[N];
+int total_money, total_per;
+
+void dfs(int u) {
+	vis[u] = true;
+	total_money += money[u - 1];
+	total_per++;
+	for(auto v: graph[u]) {
+		if(!vis[v])
+			dfs(v);
+	}
+}
+
+void solve(int cs) {
+	int n, m; cin >> n >> m;
+	money.clear();
+	for(int i = 1; i <= n; i++) {
+		vis[i] = false;
+		graph[i].clear();
+	}
+
+	int sum = 0;
+	for(int i = 0; i < n; i++) {
+		int x; cin >> x;
+		money.push_back(x);
+		sum += x;
+	}
+
+	while(m--) {
+		int u, v; cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+    cout << "Case " << cs << ": ";
+    if(sum % n != 0) {
+    	cout << "No\n";
+    	return;
+    }
+
+    set<int> s;
+    for(int u = 1; u <= n; u++) {
+    	if(!vis[u]) {
+    		total_money = total_per = 0;
+    		dfs(u);
+    		if(total_money % total_per == 0) {
+    			s.insert(total_money % total_per);
+    		}
+    		else {
+    			cout << "No\n";
+    			return;
+    		}
+    	}
+    }
+
+    if(s.size() == 1) {
+    	cout << "Yes\n";
+    	return;
+    }
+    cout << "No\n";  
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1, cs = 0;
+    cin >> t;
+    while(t--) {
+        solve(++cs);
+    }
+    return 0;
+}
---
+++ Problems/Graph Theory/Kefa and Park.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 7;
+using ll = long long;
+
+int a[N];
+vector<int> graph[N];
+bool vis[N];
+int ans = 0;
+
+void dfs(int u, int cn, int m) {
+	vis[u] = true;
+	if(a[u] == 1) {
+		cn++;
+	}
+	else {
+		cn = 0;
+	}
+	if(cn > m) {
+		return;
+	}
+
+	bool leaf = true;
+	for(auto v: graph[u]) {
+		if(!vis[v]) {
+			leaf = false;
+			dfs(v, cn, m);
+		}
+	}
+
+	if(leaf)
+		ans++;
+}
+void solve() {
+    int n, m; cin >> n >> m;
+    for(int i = 1; i <= n; i++) {
+    	cin >> a[i];
+    }
+
+    for(int i = 1; i < n; i++) {
+    	int u, v; cin >> u >> v;
+    	graph[u].push_back(v);
+    	graph[v].push_back(u);
+    }
+
+    dfs(1, 0, m);
+
+    cout << ans;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
